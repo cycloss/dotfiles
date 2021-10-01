fe302e02ef52d3fbc6283dc627662f6509f21cb1
Ccyclos <50776059+lucas979797@users.noreply.github.com>
2021-10-01 09:41:25 +0100

    Adds docker abbrs and new vscode exts

--- .config/fish/functions/myip.fish
+++ .config/fish/functions/myip.fish
@@ -1,4 +1,5 @@
-function myip -d 'Displays all ip addresses for this machine'
+# Defined in /var/folders/l6/4wtwvg1903l93zlk1_5rzrzr0000gn/T//fish.b2a1X9/myip.fish @ line 2
+function myip --description 'Displays all ip addresses for this machine'
     set -l locals (ifconfig | grep "inet " | awk '{ print $2 }')
     printf "%-15s %s\n" 'Localhost:' $locals[1]
     printf "%-15s %s\n" 'LAN:' (test -n "$locals[2]" && echo $locals[2] || echo 'N/A')
